09f23ad23c89e64ad363e956855be17200ae4afb
mamunalbd4 <mamunalbd4@gmail.com>
2023-08-15 15:00:01 +0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,15 +1,7 @@
-import streamlit as st
-import pandas as pd
-def main():
-    st.title("Online Excel Data Import App")
-    
-    # Upload Excel file
-    uploaded_file = st.file_uploader("https://drrabangladesh-my.sharepoint.com/:x:/g/personal/mamun_arktechbd_org/EeUPMTenrYNFh4YechyHKAUBaUadsZ7MtOboEDv5zVdAIQ?e=U5SUry", type=["xlsx", "xls"])
-    
-    if uploaded_file is not None:
-        df = pd.read_excel(uploaded_file)
-        st.dataframe(df)
-        
-if __name__ == "__main__":
-    main()
+import requests
 
+url = 'https://drrabangladesh-my.sharepoint.com/:x:/g/personal/mamun_arktechbd_org/EeUPMTenrYNFh4YechyHKAUBaUadsZ7MtOboEDv5zVdAIQ?e=MZgv4C'
+response = requests.get(url)
+
+with open('data.xlsx', 'wb') as f:
+    f.write(response.content)
